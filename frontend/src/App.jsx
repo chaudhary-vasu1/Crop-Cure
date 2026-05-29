2b20f288ec57b5b1719bc403a8ae991b16f7a075
chaudhary-vasu1 <choudharyvasu2705@gmail.com>
2026-05-30 00:31:53 +0530

    Wrap Dashboard in Layout component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
+
+// Pages & Components
 import Login from './pages/Login';
 import Register from './pages/Register';
-
-// IMPORT YOUR REAL DASHBOARD HERE!
-// (If you saved it in the components folder, change this to './components/Dashboard')
+import Layout from './components/Layout'; 
 import Dashboard from './pages/Dashboard'; 
 
 function App() {
@@ -13,18 +13,24 @@ function App() {
         <AuthProvider>
             <Router>
                 <Routes>
+                    {/* Public Routes */}
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} /> 
                     
-                    {/* Protected Routes */}
+                    {/* Protected Routes wrapped in the Sidebar Layout */}
                     <Route 
                         path="/" 
                         element={
                             <ProtectedRoute>
-                                <Dashboard />
+                                <Layout />
                             </ProtectedRoute>
                         } 
-                    />
+                    >
+                        {/* The 'index' route loads Dashboard automatically at '/' */}
+                        <Route index element={<Dashboard />} />
+                        
+                        {/* We will add more pages here in the next phase! */}
+                    </Route>
                 </Routes>
             </Router>
         </AuthProvider>
